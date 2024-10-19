a34d8ae1cfca882a08e7d0bbdbbed470689dc553
Nabil Khalid <holanabilkhalid@gmail.com>
2024-10-19 19:46:19 +0800

    vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { inject } from '@vercel/analytics';
 import './index.css';
+
+inject();
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
