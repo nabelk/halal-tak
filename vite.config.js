f609e9b8eb05baa02df85a3c00933e2de8812e84
Nabil Khalid <holanabilkhalid@gmail.com>
2024-11-03 03:58:23 +0800

    chore: unistall vite ssr plugin

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import vercel from 'vite-plugin-vercel';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), vercel()],
+    plugins: [react()],
     ssr: {
         external: ['react-router-dom'],
     },
