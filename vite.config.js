23bf813c34c2c448f4dac2ab8703d7d5475ad83c
Nabil Khalid <holanabilkhalid@gmail.com>
2024-11-03 02:49:28 +0800

    resolve: vercel config for vite ssr

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
+import 'dotenv/config';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import vercel from 'vite-plugin-vercel';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), vercel()],
     ssr: {
         external: ['react-router-dom'],
+    },
+    server: {
+        port: process.env.PORT,
     },
     resolve: {
         alias: {
